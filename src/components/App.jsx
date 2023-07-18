0fd5650a5b0ebdcbe708b299eef3be1b47a401c7
kristenmazza <kbmazza@gmail.com>
2023-07-18 14:43:06 -0700

    Add demo information

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [number, setNumber] = useState("(555) 555-2535");
   const [email, setEmail] = useState("demo@gmail.com");
   const [location, setLocation] = useState("Los Angeles, CA");
-  const [summary, setSummary] = useState("Results-oriented and compassionate psychologist with a proven track record in providing exceptional psychological services. Possesses a deep understanding of diverse learning needs, disabilities, and behavioral challenges. Seeking a rewarding opportunity in a private practice setting to apply my expertise and dedication to empowering students and fostering their well-being.")
+  const [summary, setSummary] = useState("Results-oriented and compassionate psychologist with a proven track record in providing exceptional psychological services. Seeking a rewarding opportunity in a private practice setting to apply my expertise and dedication to empowering students and fostering their well-being.")
 
   const initialEducationEntries = [
     {
@@ -25,6 +25,14 @@ function App() {
       school: "University of California, Sunnydale",
       schoolLocation: "Sunnydale, CA",
       schoolDegree: "Bachelor of Science in Psychology",
+      schoolDateStart: "Aug. 2016",
+      schoolDateEnd: "Aug. 2018",
+      id: crypto.randomUUID(),
+    },
+      {
+      school: "Sunnydale College",
+      schoolLocation: "Sunnydale, CA",
+      schoolDegree: "Associate of Arts in Child Development",
       schoolDateStart: "Aug. 2014",
       schoolDateEnd: "Aug. 2018",
       id: crypto.randomUUID(),
@@ -52,11 +60,29 @@ function App() {
     },
     {
       jobTitle: "Crew Member",
-      jobDateStart: "Jan. 2013",
+      jobDateStart: "Jan. 2014",
       jobDateEnd: "Apr. 2015",
       jobCompany:"Doublemeat Palace",
       jobLocation: "Sunnydale, CA",
       jobDescription: "Successfully contributed to the efficient operation of a fast-paced restaurant by delivering exceptional customer service and maintaining high standards of cleanliness and food preparation.",
+      id: crypto.randomUUID(),
+    },
+    {
+      jobTitle: "Childcare Provider",
+      jobDateStart: "Jan. 2013",
+      jobDateEnd: "Apr. 2014",
+      jobCompany:"Daily Daycare",
+      jobLocation: "Sunnydale, CA",
+      jobDescription: "Provided attentive supervision and maintained a secure and nurturing environment for children aged infants to toddlers within a dynamic daycare facility.",
+      id: crypto.randomUUID(),
+    },
+      {
+      jobTitle: "Babysitter",
+      jobDateStart: "Dec. 2011",
+      jobDateEnd: "Mar. 2013",
+      jobCompany:"Babysitter Club",
+      jobLocation: "Sunnydale, CA",
+      jobDescription: "Established a trusting and reliable relationship with parents and children as a dedicated member of the Babysitter Club, ensuring a safe and engaging environment during evening and weekend childcare sessions.",
       id: crypto.randomUUID(),
     }
   ]
